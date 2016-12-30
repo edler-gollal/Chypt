b0f8ec401a07eaa9c9c91f8e7fa0239838a159de
Lele <lelehallo@gmail.com>
2016-12-30 18:04:46 +0100

    Added errorlog

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Chat = require('./chat')(io);
+var fs = require('fs');
 
 app.use(express.static('htdocs'));
 
@@ -13,4 +14,19 @@ if(process.argv[2] != undefined) {
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
+});
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+
+  var d = new Date();
+  var hours = d.getHours();
+  if(hours < 10) hours = "0" + hours;
+  var minutes = d.getMinutes();
+  if(minutes < 10) minutes = "0" + minutes;
+  var time = hours + ":" + minutes;
+
+  fs.appendFile(__dirname + '/log.txt', time + " - " + err + "\n", function(e){
+    if(e) console.log(e);
+  })
 });
